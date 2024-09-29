a43303ea90ee46dcf820e4a0501b9f67ad10b049
ZhuGuorong <zgr2484101884@163.com>
2024-09-29 18:08:53 +0800

    使用动态代理的方式取代静态代理

--- example-consumer/src/main/java/cn/edu/szu/example/consumer/EasyConsumerExample.java
+++ example-consumer/src/main/java/cn/edu/szu/example/consumer/EasyConsumerExample.java
@@ -2,11 +2,12 @@ package cn.edu.szu.example.consumer;
 
 import cn.edu.szu.example.common.model.User;
 import cn.edu.szu.example.common.service.UserService;
+import cn.edu.szu.myrpc.proxy.ServiceProxyFactory;
 
 public class EasyConsumerExample {
     public static void main(String[] args) {
         // 获取代理对象
-        UserService userService = new UserServiceProxy();
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
 
         // 进行RPC
         User user = new User();
